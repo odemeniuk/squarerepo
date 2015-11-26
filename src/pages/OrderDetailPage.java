3e138664b64002f9ed3c446fa65d0ef75f24bea8
94117nl <rttededersixtwo@gmail.com>
2015-11-26 17:31:06 +0200

    Added OrderDetailsPage

---
+++ src/pages/OrderDetailPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+public class OrderDetailPage extends AbstractContainer{
+	static String url = "http://automationpractice.com/index.php?controller=my-account";
+    By OrderHistoryAndDetailsButton = By.cssSelector("");
+    By MyCreditSlipsButton = By.cssSelector("");
+    By MyAddressesButton = By.cssSelector("");
+    By MyPersonalInformationButton = By.cssSelector("");
+    By MyWishListsButton = By.cssSelector("");
+    By HomeButton = By.cssSelector("");
+}
