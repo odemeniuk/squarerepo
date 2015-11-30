97b89ff44d82d8aaf8ea8c943fa2db5571eec419
94117nl <rttededersixtwo@gmail.com>
2015-11-30 15:38:22 -0800

    Changed to MyAccountPage

--- src/pages/OrderDetailPage.java
+++ src/pages/OrderDetailPage.java
@@ -1,11 +1,19 @@
 package pages;
 
-public class OrderDetailPage extends AbstractContainer{
-	static String url = "http://automationpractice.com/index.php?controller=my-account";
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class MyAccountPage extends AbstractPage{
+    static String url = "http://automationpractice.com/index.php?controller=my-account";
     By OrderHistoryAndDetailsButton = By.cssSelector("");
     By MyCreditSlipsButton = By.cssSelector("");
     By MyAddressesButton = By.cssSelector("");
     By MyPersonalInformationButton = By.cssSelector("");
     By MyWishListsButton = By.cssSelector("");
     By HomeButton = By.cssSelector("");
+    
+	public MyAccountPage(WebDriver driver){
+		super(driver);
+	}
+    
 }
