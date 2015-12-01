acea63fdf09123222899c40b963a27a264dd36ad
94117nl <rttededersixtwo@gmail.com>
2015-11-30 23:01:09 -0800

    Added AbstractPage

---
+++ src/pages/AbstractPage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AbstractPage{
+	
+	protected WebDriver driver;
+	
+	public AbstractPage(WebDriver driver){
+		this.driver = driver;
+	}
+	
+	public WebDriver getDriver(){
+		return driver;
+	}
+
+}
