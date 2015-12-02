cdeadc1cf63d13170a69cb8deabd817ee8050647
94117nl <rttededersixtwo@gmail.com>
2015-12-02 20:03:02 +0200

    Extended MyAccountPage

--- src/pages/MyAccountPage.java
+++ src/pages/MyAccountPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 public class MyAccountPage extends AbstractPage{
     static String url = "http://automationpractice.com/index.php?controller=my-account";
-    By OrderHistoryAndDetailsButton = By.cssSelector("");
+    By OrderHistoryAndDetailsButton = By.cssSelector("a[title=\"Orders\"]");
     By MyCreditSlipsButton = By.cssSelector("");
     By MyAddressesButton = By.cssSelector("");
     By MyPersonalInformationButton = By.cssSelector("");
@@ -15,5 +15,34 @@ public class MyAccountPage extends AbstractPage{
 	public MyAccountPage(WebDriver driver){
 		super(driver);
 	}
+	
+	public OrderHistoryPage clickOnOrderHistoryAndDetails(){
+		driver.findElement(OrderHistoryAndDetailsButton).click();
+		return new OrderHistoryPage(driver);
+	}
+	
+	public OrderSlipPage clickOnMyCreditSlips(){
+		driver.findElement(MyCreditSlipsButton).click();
+		return new OrderSlipPage(driver);
+	}
+	
+	public AddressesPage clickOnMyAddresses(){
+		driver.findElement(MyAddressesButton).click();
+		return new AddressesPage(driver);
+	}
+	
+	public IdentityPage clickOnMyPersonalInformation(){
+		driver.findElement(MyPersonalInformationButton).click();
+		return new IdentityPage(driver);
+	}
+	
+	public MyWishlistPage clickOnMyWishLists(){
+		driver.findElement(MyWishListsButton).click();
+		return new MyWishlistPage(driver);
+	}
+	
+	//click on Home button below
+	
+	//click on Home icon above
     
 }
