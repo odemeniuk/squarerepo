ef48121610a94c78a0779e139bc43e0f9266d6a9
94117nl <rttededersixtwo@gmail.com>
2015-12-02 20:16:44 +0200

    Extended my test

--- src/tests/nlTest.java
+++ src/tests/nlTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.junit.Test;
+import org.testng.Assert;
 
 import utils.AbstractContainer;
 import pages.LoginPage;
@@ -12,6 +13,8 @@ public class nlTest extends AbstractContainer{
 	public void testMethod(){
 		LoginPage onLoginPage = new LoginPage(driver);
 		MyAccountPage onMyAccountPage = onLoginPage.loginWithCredentials("rttededer69@mai.ua", "KJhnjre7886%4");
+		onMyAccountPage.clickOnOrderHistoryAndDetails();
+		Assert.assertTrue(driver.getCurrentUrl().equals("http://automationpractice.com/index.php?controller=history"));
 	}
 	
 }
